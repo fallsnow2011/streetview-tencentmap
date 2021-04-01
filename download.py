ab6109909952d9d923fb6d6b37599cececfb6a84
fallsnow <56485723+fallsnow2011@users.noreply.github.com>
2021-04-01 22:17:54 +0800

    之前出现错误，这个为改正版

--- download.py
+++ download.py
@@ -4,7 +4,7 @@
 import urllib.request
 import os
 import socket
-import request
+import requests
 socket.setdefaulttimeout(60)
 def download(url, name):
     try:
@@ -16,13 +16,13 @@ def download(url, name):
     except:
         download(url,name)
 keys= ""
-fp = open("C:\\tengxun.csv","r",encoding='utf-8')#以只读方式打开文件。文件的指针将会放在文件的开头。这是默认模式。
+fp = open("C:\\Users\\Administrator\\Desktop\\2.csv","r",encoding='utf-8')#以只读方式打开文件。文件的指针将会放在文件的开头。这是默认模式。修改你的文件地址
 for line in fp.readlines():
      print(line)
      jin = line.split(',')[0].strip()
      wei = line.split(',')[1].strip()
     # headings = line.split(',')[2].strip()
-     path = "C:\\streetview"+ jin + "_" + wei
+     path = "D:\\streetview"+ jin + "_" + wei  #修改到你的地址
      isExists = os.path.exists(path)
      if not isExists:
         os.makedirs(path)
@@ -31,8 +31,8 @@ for line in fp.readlines():
      start_heading = 0
      for j in range(6):
          headings = (start_heading + j * 60)
-         name = path + '/' +jin + "_" + wei + "_" + headings + ".jpg"
-         url = "https://apis.map.qq.com/ws/streetview/v1/image?size=512x400&location=" + wei + "," + jin + "&heading=" +headings + "&pitch=0" + '&key=keys'
+         name = path + '/' +str(jin) + "_" + str(wei) + "_" + str(headings) + ".jpg"
+         url = "https://apis.map.qq.com/ws/streetview/v1/image?size=512x400&location=" + str(wei) + "," + str(jin) + "&heading=" +str(headings) + "&pitch=0" + '&key=填你的KEY'
          print(name)
          print(url)
          if os.path.isfile(name):
